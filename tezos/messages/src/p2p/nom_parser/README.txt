a1151e108e735360598844f44b240586f31f6b64
Vivek Pandya <vivekvpandya@gmail.com>
2021-01-18 15:19:35 +0530

    Minor clean up in tests and adding README

---
+++ tezos/messages/src/p2p/nom_parser/README.txt
@@ -0,0 +1,10 @@
+To Test correctness tests from messages directory and on branch vivek_assignment use following command:
+
+cargo test p2p::nom_parser::parse::
+
+To run new benchmark (watch for from_nom_parser )
+
+cargo bench
+
+
+
--- tezos/messages/src/p2p/nom_parser/parse.rs
+++ tezos/messages/src/p2p/nom_parser/parse.rs
@@ -2,6 +2,7 @@ use failure::Error;
 use nom::number::streaming::{be_i8, be_u8, be_u16, be_u32};
 use nom::{IResult, Err};
 use nom::error::ErrorKind;
+use nom::Err::Failure;
 use crypto::hash::HashType;
 
 use crate::p2p::encoding::prelude::*;
@@ -39,8 +40,7 @@ named!(opp<Operation>,
     (opss)
     )
 );
-            // length_value!(be_u32,
-            //     operation)
+
 named!(operations<Vec<Operation>>,
     do_parse!(
         ops: many0!(complete!(length_value!(be_u32, operation)))
@@ -52,7 +52,7 @@ named!(operations<Vec<Operation>>,
 named!(operation_for_blocks_message<PeerMessage>,
     do_parse!(
         ops_for_block: operation_for_block >>
-        ops_hashes_path: path_encoding >>
+        ops_hashes_path: call!(path_encoding_depth, 0) >>
         ops: alt!(many0!(
             complete!(
             length_value!(be_u32,
@@ -229,8 +229,20 @@ fn get_op_for_block() -> Result<(), Error> {
 #[test]
 fn get_ops_for_block() -> Result<(), Error> {
    let bh = hex::decode("00000084ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa01ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa02ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa00ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa03")?;
- let ops = get_operations_for_block(&bh);
+ let ops = get_operations_for_block(&bh).unwrap().1;
  println!("operations for block: {:?}", &ops);
+    match ops {
+        PeerMessage::GetOperationsForBlocks(message) => {
+            let operations = message.get_operations_for_blocks();
+            assert_eq!(4, operations.len());
+            assert_eq!(
+                    "BMWmj9CTojf7AnA8ZQFWGkh1cXB6FkST8Ey5coaeHX6cVNAZqA6",
+                    HashType::BlockHash.hash_to_b58check(operations[0].hash())
+                    );
+            assert_eq!(1, operations[0].validation_pass());
+        }
+         _ => panic!("Unsupported encoding: {:?}", ops),
+    }
     Ok(())
 }
 
@@ -253,12 +265,15 @@ fn get_peer_message_response() -> Result<(), Error> {
         }
          _ => panic!("Unsupported encoding: {:?}", message),
     }
+    Ok(())
+}
+
+#[test]
+fn wrong_message() -> Result<(), Error> {
         let wrong = hex::decode("0000098a006000000084ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa01ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa02ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa00ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa03")?;
     let wrong_message = peer_message_response(&wrong);
+    assert!(wrong_message.is_err());
     println!("{:?}", wrong_message);
-        // let wrong2 = hex::decode("0000008a00600000084ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa01ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa02ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa00ed4197d381a4d4f56be30bf7157426671276aa187bbe0bb9484974af59e069aa03")?;
-    // let wrong_message2 = peer_message_response(&wrong2);
-    // println!("{:?}", wrong_message2);
     Ok(())
 }
 
@@ -267,12 +282,6 @@ fn get_operations_for_blocks_right() -> Result<(), Error> {
         let message_bytes = hex::decode("000000660061b12238a7c3577d725939970800ade6b82d94a231e855b46af46c37850dd02452030ffe7601035ca2892f983c10203656479cfd2f8a4ea656f300cd9d68f74aa625870f7c09f7c4d76ace86e1a7e1c7dc0a0c7edcaa8b284949320081131976a87760c300")?;
 let pmessage = peer_message_response(&message_bytes).unwrap().1;
 println!("{:?}" , pmessage);
-        let b = hex::decode("000000660061b12238a7c3577d725939970800ade6b82d94a231e855b46af46c37850dd0245203")?;
-        let d = dynamic(&b);
-        println!("{:?}", d);
-        let b1 = hex::decode("b12238a7c3577d725939970800ade6b82d94a231e855b46af46c37850dd02452030ffe7601035ca2892f983c10203656479cfd2f8a4ea656f300cd9d68f74aa625870f7c09f7c4d76ace86e1a7e1c7dc0a0c7edcaa8b284949320081131976a87760c300")?;
-let e = operation_for_blocks_message(&b1);
-println!("{:?}" , e);
 assert_eq!(1, pmessage.messages().len());
 
 let message = pmessage.messages().get(0).unwrap();
@@ -316,12 +325,8 @@ match message {
 fn get_operations_for_blocks_left() -> Result<(), Error> {
     let message_bytes = hex::decode("0000027300613158c8503e7cd436d09a8a6320cd57014870a96f178915be25551e435d0830ab00f0f0007c09f7c4d76ace86e1a7e1c7dc0a0c7edcaa8b284949320081131976a87760c30a37f18e2562ae14388716247be0d4e451d72ce38d1d4a30f92d2f6ef95b4919000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6d1852a1f397619b16f08121fb01d43a9bf4ded283ab0d96fd114028251690506a7ec514f0b297b6cdc8ff54a658f27f7635d201c61479cd48007c0096752fb0c000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb62b8768820e6b7343c32382544d0fa0f044289fd1b86ee5c66e36396bc9bc2492314543667770959449943d222ffd7f7cd8e3ad8eda9d21a8a5e9e34c73c0c9e3000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c5d4ac0ba67f6509fec4ae196d1cb7ccf8ee7a35bc06d362d69291631a5a07b511252c70d59ff94dc4071525dd6c22354349702c9821d80c748a15913f11b1d1000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb63d61de83c6f71ca631903f29be9040f63dbf5d00d7994a8420210270aa2c37e245ce70e8f4d7d384f342f7e6b6797c5f237ae1846a8b8652838663d1d0df91a0000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c69c651e14357c3a895cd6465fc1e3b1fd19b0d805efae484f2632e006101b9c80c28c92dcfbf58b99392b2108b286fd28039ddd72294929c2fbf9dda65acf01")?;
 
-    // let b = hex::decode("3158c8503e7cd436d09a8a6320cd57014870a96f178915be25551e435d0830ab00f0f0007c09f7c4d76ace86e1a7e1c7dc0a0c7edcaa8b284949320081131976a87760c30a37f18e2562ae14388716247be0d4e451d72ce38d1d4a30f92d2f6ef95b4919000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6d1852a1f397619b16f08121fb01d43a9bf4ded283ab0d96fd114028251690506a7ec514f0b297b6cdc8ff54a658f27f7635d201c61479cd48007c0096752fb0c000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb62b8768820e6b7343c32382544d0fa0f044289fd1b86ee5c66e36396bc9bc2492314543667770959449943d222ffd7f7cd8e3ad8eda9d21a8a5e9e34c73c0c9e3000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c5d4ac0ba67f6509fec4ae196d1cb7ccf8ee7a35bc06d362d69291631a5a07b511252c70d59ff94dc4071525dd6c22354349702c9821d80c748a15913f11b1d1000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb63d61de83c6f71ca631903f29be9040f63dbf5d00d7994a8420210270aa2c37e245ce70e8f4d7d384f342f7e6b6797c5f237ae1846a8b8652838663d1d0df91a0000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c69c651e14357c3a895cd6465fc1e3b1fd19b0d805efae484f2632e006101b9c80c28c92dcfbf58b99392b2108b286fd28039ddd72294929c2fbf9dda65acf01")?;
 let pmessage = peer_message_response(&message_bytes).unwrap().1;
 println!("{:?}" , pmessage);
-// let ops = operation_for_blocks_message1(&b);
-// println!("{:?}", ops);
-// Ok(())
     assert_eq!(1, pmessage.messages().len());
 
     let message = pmessage.messages().get(0).unwrap();
@@ -370,17 +375,17 @@ println!("{:?}" , pmessage);
 #[test]
 fn get_operations1() -> Result<(), Error> {
 let b = hex::decode("000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6d1852a1f397619b16f08121fb01d43a9bf4ded283ab0d96fd114028251690506a7ec514f0b297b6cdc8ff54a658f27f7635d201c61479cd48007c0096752fb0c000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb62b8768820e6b7343c32382544d0fa0f044289fd1b86ee5c66e36396bc9bc2492314543667770959449943d222ffd7f7cd8e3ad8eda9d21a8a5e9e34c73c0c9e3000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c5d4ac0ba67f6509fec4ae196d1cb7ccf8ee7a35bc06d362d69291631a5a07b511252c70d59ff94dc4071525dd6c22354349702c9821d80c748a15913f11b1d1000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb63d61de83c6f71ca631903f29be9040f63dbf5d00d7994a8420210270aa2c37e245ce70e8f4d7d384f342f7e6b6797c5f237ae1846a8b8652838663d1d0df91a0000000658a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6c69c651e14357c3a895cd6465fc1e3b1fd19b0d805efae484f2632e006101b9c80c28c92dcfbf58b99392b2108b286fd28039ddd72294929c2fbf9dda65acf01")?;
-let ops = operations(&b);
+let ops = operations(&b).unwrap().1;
+assert_eq!(5, ops.len());
 println!("{:?}", ops);
-// let bo = hex::decode("8a7912f9de23a446748861d2667ffa3b4463ed236689492c74703cef598e6f3f0000002eb6d1852a1f397619b16f08121fb01d43a9bf4ded283ab0d96fd114028251690506a7ec514f0b297b6cdc8ff54a658f27f7635d201c61479cd48007c0096752fb0c")?;
-// let o = operation(&bo);
-// println!("{:?}", o);
 Ok(())
 }
+
 #[test]
 fn left_recursion() -> Result<(), Error> {
 let b = hex::decode("f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0")?;
 let p = path_encoding_depth(&b, 0);
+assert_eq!(p, Err(Err::Failure(nom::error::Error { input: &b[21..], code: ErrorKind::TooLarge})));
 println!("{:?}", p);
 Ok(())
 }
